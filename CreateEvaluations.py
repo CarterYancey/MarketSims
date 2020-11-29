35adfdc7f7fef077536c7a56ef65654315aea44d
CarterYancey <cdyancey@crimson.ua.edu>
2020-11-29 16:16:07 -0600

    Now supports CL arguments

--- CreateEvaluations.py
+++ CreateEvaluations.py
@@ -2,9 +2,29 @@ import json
 import pandas as pd
 import numpy as np
 import statistics as stats
+import os
+import sys, getopt
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-path="C:/Users/carte/MarketSims/SP500/"
-file="SP500.txt"
+path = os.path.dirname(os.path.realpath(__file__))+'/'
+file=''
+try:
+    opts, args = getopt.gnu_getopt(sys.argv[1:], 'hi:p:')
+except getopt.GetoptError as err:
+    print(str(err))
+    print('SymbolPerformance.py -h for usage')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in("-h", "--help"):
+        print(' -i <input file> \t Filename of file that contains list of symbols seperated by newlines. Must be in same directory as this script, unless the -p flag is used.')
+        print(' -p, --path=<directory> \t Directory for this script to work in. Working directory by default.')
+        sys.exit(2)
+    elif opt in ('-i', "--infile"):
+        file = arg
+    elif opt in ('-p', "--path"):
+        path = arg+'/'
+if (file==''):
+    print('Must have input file. See SymbolPerformance.py -h for usage')
+    sys.exit(2)
 symbols = [line.rstrip('\n') for line in open(path+file)]
 symbol_analysis = {}
 skipped = []
