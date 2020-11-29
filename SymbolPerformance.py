1b9287d1330937a1e4e98fc5f37a2b89df7b5916
CarterYancey <cdyancey@crimson.ua.edu>
2020-11-29 15:53:30 -0600

    Now supports the use of CL arguments

--- SymbolPerformance.py
+++ SymbolPerformance.py
@@ -2,10 +2,31 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import seaborn
 import pandas as pd
+import os
+import sys, getopt
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-path="C:/Users/carte/MarketSims/SP500/"
-file="SP500.txt"
+path = os.path.dirname(os.path.realpath(__file__))+'/'
+file=''
+try:
+    opts, args = getopt.gnu_getopt(sys.argv[1:], 'hi:p:')
+except getopt.GetoptError as err:
+    print(str(err))
+    print('SymbolPerformance.py -h for usage')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in("-h", "--help"):
+        print(' -i <input file> \t Filename of file that contains list of symbols seperated by newlines. Must be in same directory as this script, unless the -p flag is used.')
+        print(' -p, --path=<directory> \t Directory for this script to work in. Working directory by default.')
+        sys.exit(2)
+    elif opt in ('-i', "--infile"):
+        file = arg
+    elif opt in ('-p', "--path"):
+        path = arg+'/'
+if (file==''):
+    print('Must have input file. See SymbolPerformance.py -h for usage')
+    sys.exit(2)
+
 symbols = [line.rstrip('\n') for line in open(path+file)]
 data_df = yf.download(tickers=symbols, period='9mo', interval='1d', rounding='True')
 data_df = data_df['Close']
