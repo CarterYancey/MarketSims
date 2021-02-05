14160c5fa515b8cb16e27a77af67b33f2f5d32bb
CarterYancey <cdyancey@crimson.ua.edu>
2021-02-04 22:54:29 -0600

    Added support for historical evaluations

--- CreateEvaluations.py
+++ CreateEvaluations.py
@@ -4,13 +4,16 @@ import numpy as np
 import statistics as stats
 import os
 import sys, getopt
+import yfinance as yf
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 path = os.path.dirname(os.path.realpath(__file__))+'/'
 file=''
+historicString='_'
+historicAnalysis=False
 
 #Read CL arguments
 try:
-    opts, args = getopt.gnu_getopt(sys.argv[1:], 'hi:p:o:')
+    opts, args = getopt.gnu_getopt(sys.argv[1:], 'hi:p:o:H')
 except getopt.GetoptError as err:
     print(str(err))
     print('SymbolPerformance.py -h for usage')
@@ -20,6 +23,7 @@ for opt, arg in opts:
         print(' -i <input file> \t Filename of file that contains list of symbols seperated by newlines. Must be in same directory as this script, unless the -p flag is used.')
         print(' -p, --path=<directory> \t Directory for this script to work in. Working directory by default.')
         print(' -o <outfile> \t Filename of a file that will be written to rather than stdout. This only affects what is normally printed to the terminal.')
+        print(' -H, --historic \t Perform analysis using oldest data available, as if you were using this script in the past. Helpful for determining efficacy of this script. BEWARE SURVIVORSHIP BIAS!')
         sys.exit(2)
     elif opt in ('-i', "--infile"):
         file = arg
@@ -28,6 +32,9 @@ for opt, arg in opts:
     elif opt in ('-o'):
         outfile = path+arg
         sys.stdout = open(outfile, 'w')
+    elif opt in ('-H', "--historic"):
+        historicAnalysis = True
+        historicString = '_Historic'
 if (file==''):
     print('Must have input file. See SymbolPerformance.py -h for usage')
     sys.exit(2)
@@ -39,8 +46,11 @@ skipped = []
 maxInt = 2**63 - 1
 sciNot='{:.2E}' #Scientific Notation
 
+badMessages = []
 def badDataMessage(data, symbol):
-    print("***INSUFFICIENT "+data+" DATA FOR MANAGEMENT ANALYSIS of "+symbol+" ***\n")
+    messageStr = ("***INSUFFICIENT "+data+" DATA FOR ANALYSIS of "+symbol+" ***\n")
+    print(messageStr)
+    badMessages.append(messageStr)
     
 print("We will take a list of symbols and evaluate the company using their financial documentation.\n")
 print("Important numbers:")
@@ -60,50 +70,63 @@ for symbol in symbols:
             continue
         if (price == 0):
             skipped.append(symbol)
+            badDataMessage("Price", symbol)
             continue
         description = profile[0]['description'] if ('description' in profile[0]) else "[n/a]"
         dividend = profile[0]['lastDiv']/price*100 if ('lastDiv' in profile[0]) else 0
     with open(path+symbol+'quarterlyKeyMetrics.json', 'r') as read_file:
-        quarterly = json.load(read_file)
-        quarters = len(quarterly)
-        quarterly_bookValuePerShare = [quarterly[n]['bookValuePerShare'] for n in range(quarters-1,-1,-1)]
-        quarterly_netIncomePerShare = [quarterly[n]['netIncomePerShare'] for n in range(quarters-1,-1,-1)]
+        quarterlyData = json.load(read_file)
+        if (historicAnalysis):
+            quarterlyData = quarterlyData[-10:]
+        numQuarters = min(len(quarterlyData), 10) #Use at most last 10 quarters of data
+        quarterly = {}
+        RANGE = range(numQuarters-1,-1,-1) #We want our list to end in the present, but quarterlyData[0] is most recent.
+        metrics = ['bookValuePerShare', 'netIncomePerShare']
+        for metric in metrics:
+            quarterly[metric] = [quarterlyData[n][metric] for n in RANGE]
         try:
-            bvPerShare = stats.mean(quarterly_bookValuePerShare[-4:])
+            bvPerShare = stats.mean(quarterly['bookValuePerShare'][-4:])
         except:
             badDataMessage("quarterly BV/share", symbol)
             bvPerShare = 0
             #THIS NEEDS DRAMATIC IMPROVEMENT BUT IT IS ALMOST BEDTIME
         try:
-            quarterlyEPS = stats.mean(quarterly_netIncomePerShare[-4:])
+            quarterlyEPS = stats.mean(quarterly['netIncomePerShare'][-4:])
         except:
             badDataMessage("quarterly EPS", symbol)
             quarterlyEPS = 0
             #THIS NEEDS DRAMATIC IMPROVEMENT BUT IT IS ALMOST BEDTIME
     with open(path+symbol+'annualKeyMetrics.json', 'r') as read_file:
-        annual = json.load(read_file)
-        years = len(annual)
-        annual_bookValuePerShare = [annual[n]['bookValuePerShare'] for n in range(years-1,-1,-1)]
-        annual_netIncomePerShare = [annual[n]['netIncomePerShare'] for n in range(years-1,-1,-1)]
+        annualData = json.load(read_file)
+        if (historicAnalysis):
+            annualData = annualData[-10:]
+        numYears = min(10, len(annualData)) #User at most last 10 years of data
+        RANGE = range(numYears-1,-1,-1)
+        annual = {}
+        for metrics in metrics:
+            annual[metric] = [annualData[n][metric] for n in RANGE]
         try:
-            annualEPS = stats.mean(annual_netIncomePerShare[-3:])
+            annualEPS = stats.mean(annual['netIncomePerShare'][-3:])
         except:
             badDataMessage("annual EPS", symbol)
             annualEPS = 0
             #THIS NEEDS DRAMATIC IMPROVEMENT BUT IT IS ALMOST BEDTIME
+    #Source quarterly Balance Sheet Data
     with open(path+symbol+'quarterlyBalanceSheet.json', 'r') as read_file:
-        quarterly = json.load(read_file)
-        quarters = len(quarterly)
-        quarterly_dates = [quarterly[n]['date'] for n in range(quarters-1,-1,-1)]
-        quarterly_cash = [quarterly[n]['cashAndCashEquivalents'] for n in range(quarters-1,-1,-1)]
-        quarterly_shortTermDebt = [quarterly[n]['shortTermDebt'] for n in range(quarters-1,-1,-1)]
-        quarterly_netReceivables = [quarterly[n]['netReceivables'] for n in range(quarters-1,-1,-1)]
-        quarterly_longTermDebt = [quarterly[n]['longTermDebt'] for n in range(quarters-1,-1,-1)]
-        quarterly_df = pd.DataFrame([quarterly_cash, quarterly_shortTermDebt, quarterly_longTermDebt])
-        quarterly_df.columns=quarterly_dates
+        quarterlyData = json.load(read_file)
+        if (historicAnalysis):
+            quarterlyData = quarterlyData[-10:]
+        numQuarters = min(10, len(quarterlyData))
+        RANGE = range(numQuarters-1,-1,-1)
+        quarterly = {}
+        metrics = ['date', 'cashAndCashEquivalents', 'shortTermDebt', 'netReceivables', 'longTermDebt']
+        for metric in metrics:
+            quarterly[metric] = [quarterlyData[n][metric] for n in RANGE]
+        quarterly_df = pd.DataFrame([quarterly['cashAndCashEquivalents'], quarterly['shortTermDebt'], quarterly['longTermDebt']])
+        quarterly_df.columns=quarterly['date']
         quarterly_df.index=['cash', 'shortTermDebt', 'longTermDebt']
         try:
-            STDoverCASH = stats.mean(quarterly_shortTermDebt[-2:])/stats.mean(quarterly_cash[-2:])
+            STDoverCASH = stats.mean(quarterly['shortTermDebt'][-2:])/stats.mean(quarterly['cashAndCashEquivalents'][-2:])
         except ZeroDivisionError:
             STDoverCASH = maxInt
         except stats.StatisticsError:
@@ -111,29 +134,31 @@ for symbol in symbols:
             skipped.append(symbol)
             continue
         try:
-            LTDoverREC = stats.mean(quarterly_longTermDebt[-2:])/stats.mean(quarterly_netReceivables[-2:])
+            LTDoverREC = stats.mean(quarterly['longTermDebt'][-2:])/stats.mean(quarterly['netReceivables'][-2:])
         except ZeroDivisionError:
             LTDoverREC = maxInt
         except stats.StatisticsError:
             badDataMessage("Quarterly LTD", symbol)
             skipped.append(symbol)
             continue
+    #Source annual Balance Sheet Data
     with open(path+symbol+'annualBalanceSheet.json', 'r') as read_file:
-        annual = json.load(read_file)
-        years = len(annual)
-        #I should make these all dictionary entries
-        annual_dates = [annual[n]['date'] for n in range(years-1,-1,-1)]
-        annual_cash = [annual[n]['cashAndCashEquivalents'] for n in range(years-1,-1,-1)]
-        annual_netReceivables = [annual[n]['netReceivables'] for n in range(years-1,-1,-1)]
-        annual_PPE = [annual[n]['propertyPlantEquipmentNet'] for n in range(years-1,-1,-1)]
-        annual_shortTermDebt = [annual[n]['shortTermDebt'] for n in range(years-1,-1,-1)]
-        annual_longTermDebt = [annual[n]['longTermDebt'] for n in range(years-1,-1,-1)]
-        annuals = [annual_cash, annual_shortTermDebt,annual_netReceivables,
-                   annual_PPE, annual_longTermDebt]
-        annual_df = pd.DataFrame(annuals,
-                                 columns=annual_dates,
+        annualData = json.load(read_file)
+        if (historicAnalysis):
+            annualData = annualData[-10:]
+        numYears = min(10, len(annualData))
+        RANGE = range(numYears-1,-1,-1)
+        annual = {}
+        metrics = ['date', 'cashAndCashEquivalents', 'netReceivables', 'propertyPlantEquipmentNet', 'shortTermDebt', 'longTermDebt']
+        for metric in metrics:
+            annual[metric] = [annualData[n][metric] for n in RANGE]
+        annualMetrics = [annual['cashAndCashEquivalents'], annual['shortTermDebt'],annual['netReceivables'],
+                   annual['propertyPlantEquipmentNet'], annual['longTermDebt']]
+        annual_df = pd.DataFrame(annualMetrics,
+                                 columns=annual['date'],
                                  index=['cash', 'shortTermDebt','netReceivables',
                                         'PPE', 'longTermDebt'])
+    #Begin Analysis
     analysis=[STDoverCASH, LTDoverREC, dividend, bvPerShare, annualEPS, quarterlyEPS]
     analysis = [round(n, 4) for n in analysis]
     #DisplayData
@@ -143,75 +168,91 @@ for symbol in symbols:
     print(annual_df)
     print('\n')
     #Least squares for annual Longterm Debt, receivables, and NPPE.
-    if (years > 1): #Cannot do linear regression without at least 2 data points
-        a = [n for n in range(min(5, years))]
+    if (numYears > 1): #Cannot do linear regression without at least 2 data points
+        a = [n for n in range(min(5, numYears))]
         a_length = len(a)
         A = np.vstack([a, np.ones(a_length)]).T
-        m_rec, c_rec = np.linalg.lstsq(A, annual_netReceivables[-a_length:], rcond=None)[0]
-        m_ppe, c_ppe = np.linalg.lstsq(A, annual_PPE[-a_length:], rcond=None)[0]
-        m_ltd, c_ltd = np.linalg.lstsq(A, annual_longTermDebt[-a_length:], rcond=None)[0]
-        annualRECMean = stats.mean(annual_netReceivables[-a_length:])
-        annualPPEMean = stats.mean(annual_PPE[-a_length:])
-        annualLTDMean = stats.mean(annual_longTermDebt[-a_length:])
-        annualRECVar = stats.variance(annual_netReceivables[-a_length:])
-        annualPPEVar = stats.variance(annual_PPE[-a_length:])
-        annualLTDVar = stats.variance(annual_longTermDebt[-a_length:])
-        results_rec = [m_rec, c_rec, annualRECMean, annualRECMean/m_rec, annualRECVar**0.5]
-        results_ppe = [m_ppe, c_ppe, annualPPEMean, annualPPEMean/m_ppe, annualPPEVar**0.5]
-        results_ltd = [m_ltd, c_ltd, annualLTDMean, annualLTDMean/m_ltd, annualLTDVar**0.5]
+        slope = {}
+        intercept = {}
+        metrics = ['netReceivables', 'propertyPlantEquipmentNet', 'longTermDebt']
+        for metric in metrics:
+            slope[metric], intercept[metric] = np.linalg.lstsq(A, annual[metric][-a_length:], rcond=None)[0]
+        annualMeans = {}
+        annualVars = {}
+        for metric in metrics:
+            annualMeans[metric] = stats.mean(annual[metric][-a_length:])
+            annualVars[metric] = stats.variance(annual[metric][-a_length:])
+        annualResults = {}
+        for metric in metrics:
+            annualResults[metric] = [slope[metric], intercept[metric], annualMeans[metric], annualMeans[metric]/slope[metric], annualVars[metric]**0.5]
         print("\t\tm \t c \t mean \t mean/m \t mean/StdDev")
-        print("rec: ", list(map(sciNot.format, results_rec)))
-        print("ppe: ", list(map(sciNot.format, results_ppe)))
-        print("ltd: ", list(map(sciNot.format, results_ltd)))
+        for metric in metrics:
+            print(metric + ": ", list(map(sciNot.format, annualResults[metric])))
         print('\n')
-        for result in results_rec[2:]:
-            analysis.append(round(result, 4))
-        for result in results_ppe[2:]:
-            analysis.append(round(result, 4))
-        for result in results_ltd[2:]:
-            analysis.append(round(result, 4))
+        for metric in metrics:
+            try:
+                analysis.append(round(annualResults[metric][2]/annualResults[metric][4], 4))
+            except ZeroDivisionError:
+                analysis.append('nan')
+            analysis.append(round(annualResults[metric][3], 4))
     else:
         badDataMessage("Annual", symbol)
         skipped.append(symbol)
         continue
-    analysis.append((2/3)*bvPerShare)
-    analysis.append((2/3)*bvPerShare + 10*annualEPS)
-    analysis.append((2/3)*bvPerShare + 20*annualEPS)
-    #    analysis=[STDoverCASH, LTDoverREC, dividend, bvPerShare, annualEPS, quarterlyEPS]
+    analysis.append(round((2/3)*bvPerShare, 4))
+    analysis.append(round((2/3)*bvPerShare + 10*annualEPS, 4))
+    analysis.append(round((2/3)*bvPerShare + 20*annualEPS, 4))
+    annualLTDgrowth = annualResults['longTermDebt'][3]
+    annualPPEgrowth = annualResults['propertyPlantEquipmentNet'][3]
+    annualRECgrowth = annualResults['netReceivables'][3]
     rating = 0
     if (STDoverCASH < 7):
         rating +=1
     if (LTDoverREC < 7):
-        rating +=1
+        rating +=2
     if (dividend > 2.5):
+        rating +=0.5
+    #if (results_ltd[3] < 0): 
+    if (annualLTDgrowth < 0 and annualLTDgrowth > -15): 
         rating +=1
-    if (results_ltd[3] < 0):
-        rating +=1
-    if (results_ltd[3] > -5 and results_ltd[3] < 0):
-        rating +=1
-    if (results_ltd[3] >= 0):
-        if ((results_ltd[3] > results_rec[3]) and (results_rec[3] > 0)):
+        if (annualLTDgrowth > -5):
+            rating +=1
+    if (annualLTDgrowth >= 0):
+        if ((annualLTDgrowth > annualRECgrowth) and (annualRECgrowth > 0)):
             rating+=0.5
-        if ((results_ltd[3] > results_ppe[3]) and (results_ppe[3] > 0)):
+        if ((annualLTDgrowth > annualPPEgrowth) and (annualPPEgrowth > 0)):
             rating+=0.5
-    if (results_ltd[3] > 0 and results_ltd[3] < 5):
+    if (annualLTDgrowth > 0 and annualLTDgrowth < 5):
         rating -=1
-    if (results_rec[3] > 0 and results_rec[3] <5):
-        rating +=1
-    analysis.append(price)
+    if (annualRECgrowth > 0 and annualRECgrowth <5):
+        rating +=2   
     analysis.append(rating)
+    if (historicAnalysis):      
+        data_df = yf.download(tickers=symbol, start=annualData[0]['date'], rounding='True', actions=True)
+        twelvemonthLow = min(data_df['Close'][:300])
+        tenyrlater = data_df['Close'][min(3000, len(data_df)-1)]
+        analysis.append(twelvemonthLow)
+        analysis.append(tenyrlater)
+        analysis.append(price)
+        analysis.append(annualData[0]['date'])
+    else:
+        analysis.append(price)
     symbol_analysis[symbol] = analysis
 
+for message in badMessages:
+    print(message)
 print("Skipped: ",skipped)
 tablecolumns = ["STD/Cash", "LTDoverREC", "dividend",
                 "BV/share", "annualEPS", "quarterlyEPS",
-                "rec mean", "rec mean/m", "rec stdDev",
-                "ppe mean", "ppe mean/m","ppe stdDev",
-                "ltd mean", "ltd mean/m","ltd stdDev",
+                "rec mean/stdDev", "rec mean/m",
+                "ppe mean/stdDev", "ppe mean/m",
+                "ltd mean/stdDev", "ltd mean/m",
                 "Estimate1", "Estimate2", "Estimate3",
-                "Price", "Rating"]
+                "Rating", "Price"]
+if (historicAnalysis):
+    tablecolumns += ["10yrlatr", "Today", "LastDate"]
 results = pd.DataFrame.from_dict(symbol_analysis, orient='index', columns=tablecolumns)
 print(results)
 name=file.split('.')[0]
-results.to_csv(path+name+'_Analysis.csv', index=True, header=True)
-sys.stdout.close()
+results.to_csv(path+name+historicString+'Analysis.csv', index=True, header=True)
+#sys.stdout.close()
